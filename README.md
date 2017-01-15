a6748a7f16e43c795bb6bc2eee8d05c815c26422
Robert <robertquitt@gmail.com>
2017-01-14 16:56:14 -0800

    Add command-line args, update README with documentation, add gitignore

--- README.md
+++ README.md
@@ -1,37 +1,52 @@
-mkv-xml-helper
-====
-A tool I use to help me generate chapters for long audios in .mkv format from track listings.
+#mkv-xml-helper
+A tool assist in generation of chapter info for long audios in .mkv format from chapter listings on youtube videos.
+
+## An Example
+Converting [this soundtrack from YouTube](https://www.youtube.com/watch?v=ca7IELhD_mI) into a navigatable .mkv file.
+
+#### Prerequisites
+- (optional) [chocolatey](https://chocolatey.org/) for Windows systems for easy installation of the tools below
+- [youtube-dl](https://rg3.github.io/youtube-dl/)
+- [ffmpeg](http://ffmpeg.org/)
+- mkvpropedit (part of [mkvtoolnix](https://mkvtoolnix.download/))
+
+#### Instructions
+1. Find the video you want, then copy the video ID from the URL.
+2. Use `youtube-dl -F ca7IELhD_mI` to view the available formats.
+3. Download with best audio quality, usually `youtube-dl -f 22 -x ca7IELhD_mI`
+	- `-x` to extract audio
+4. Convert into Matroska format (.mkv) with `ffmpeg -i "Super Mario Galaxy - Full OST (Complete Soundtrack)-ca7IELhD_mI" -c:a copy "Super Mario Galaxy - Full OST.mkv"`
+	- `-i` specifies input
+	- `-c:a` specifies audio codec, "copy" to avoid transcoding
+5. Copy chapter listing from description into text file `mario_galaxy.txt`, confirm there are no typos.
+6. Convert to XML using `python3 chapters_to_xml.py -i 0 1 None -e 2:15:25 mario_galaxy.txt mario_galaxy.xml` 
+	- `-i 0 1 None` means `0` is the start time index, and `[1:None]` is the list slice for the chapter title. The line is tokenized by whitespace.
+	- `-e ` is the ending time, needed to specify the duration of the last chapter
+7. Apply the chapters to the .mkv by using `mkvpropedit "Super Mario Galaxy - Full OST.mkv" --chapters mario_galaxy.xml`
+8. Open the file with your favorite media player (I recommend [MPC-HC](https://mpc-hc.org/) for Windows) and enjoy!
+
+
 ## chapters_to_xml.py
-### converts track listings into xml data readable by mkvpropedit
+### Converts chapter listings into xml data readable by mkvpropedit
 
-Turns
+#### Requires:
+- lxml
+  - `pip install lxml`
 
-1. Fibre - Supernatural 0:00 - 2:32
-2. Night Tempo - Dreamer 2:33 - 5:05
-[...]
+usage: `chapters_to_xml.py [-h] [-d] [-i timestamp titlestart titleend] [-c config] -e endtime inputfile outputfile`
 
-into
-```
-<?xml version='1.0' encoding='ASCII'?>
-<Chapters>
-  <EditionEntry>
-    <ChapterAtom>
-      <ChapterTimeStart>0:00</ChapterTimeStart>
-      <ChapterTimeEnd>2:32</ChapterTimeEnd>
-      <ChapterDisplay>
-        <ChapterString>Fibre - Supernatural</ChapterString>
-        <ChapterLanguage>eng</ChapterLanguage>
-      </ChapterDisplay>
-    </ChapterAtom>
-    <ChapterAtom>
-      <ChapterTimeStart>2:33</ChapterTimeStart>
-      <ChapterTimeEnd>5:05</ChapterTimeEnd>
-      <ChapterDisplay>
-        <ChapterString>Night Tempo - Dreamer</ChapterString>
-        <ChapterLanguage>eng</ChapterLanguage>
-      </ChapterDisplay>
-    </ChapterAtom>
-    [...]
-  </EditionEntry>
-</Chapters>
-    ```
+Converts lines of text containing chapter information into XML readable by
+mkvpropedit.
+
+| positional arguments
+|:-|-|
+|`inputfile`  |text file where each line has a chapter name and start time
+|`outputfile` |XML file to be written to.
+
+|optional arguments
+|:-|-|
+|`-h`, `--help`          |show this help message and exit
+|`-d`, `--debug`         |enable debug printing
+|`-i timestamp titlestart titleend` | specifies the indices of the elements on each line (supports negative indices) where timestamp=tokens[timestamp] and title=tokens[titlestart:titleend]. Arguments must be integers or None. If neither -i nor -c are specified, an indexing configuration will be automatically generated.
+|`-c config`             |use a premade indexing configuration. Overrides the `-i` option. Valid configs are: super_mario_galaxy, squad_goals, pink_season, minecraft
+|`-e endtime`            |specifies the timestamp of the end of the final chapter, usually the length of the entire track.
--- chapters_to_xml.py
+++ chapters_to_xml.py
@@ -1,100 +1,151 @@
+#!/usr/bin/env python
 '''
 chapters_to_xml.py - converts track listing text into xml data for mkvpropedit
 
-ffmpeg -i file.ogg -c:a copy file.mkv
+python chapters_to_xml.py chapters.txt chapters.xml
+ffmpeg -i file. -c:a copy file.mkv
 mkvpropedit file.mkv --chapter chapters.xml
 '''
+import sys
+import argparse
+import time
 from lxml import etree
 
-INPUT_FILENAME = 'super_mario_galaxy.txt'
-OUTPUT_FILENAME = 'super_mario_galaxy.xml'
-END_TIME = '02:15:24'
+CONFIG_PRESETS = {
+    'super_mario_galaxy': (0, 1, None),
+    # "0:00 Wii Menu - Starting The Game (''Super Mario Galaxy'')" https://www.youtube.com/watch?v=ca7IELhD_mI
+    'squad_goals': (-3, 1, -3),
+    # "1. Fibre - Supernatural 0:00 - 2:32" https://www.youtube.com/watch?v=tbWS0j2fulY
+    'pink_season': (-1, 1, -1),
+    # "1. Hot Nickel Ball On A P*ssy 0:00" https://www.youtube.com/watch?v=0c_mhrB7LlQ
+    'minecraft': (0, 2, None)
+    # "0:00 - Key" https://www.youtube.com/watch?v=Dg0IjOzopYU
+}
 
-def parse_squad_goals(file):
-    """From the description of https://www.youtube.com/watch?v=tbWS0j2fulY
-    Text is of the form:
-    1. Fibre - Supernatural 0:00 - 2:32
-    2. Night Tempo - Dreamer 2:33 - 5:05
-    ...
-    """
-    for line in file:
-        tokens = line.split()
-        yield {
-            'time_start': tokens[-3],
-            'time_end': tokens[-1],
-            'chapter_string': ' '.join(tokens[1:-3])
-        }
+def time_string(s):
+    try:
+        return time.strptime(s, "%M:%S")
+    except ValueError:
+        pass
+    try:
+        return time.strptime(s, "%H:%M:%S")
+    except ValueError:
+        pass
+    return None
 
-def parse_pink_season(file):
-    """From the description of https://www.youtube.com/watch?v=0c_mhrB7LlQ
-    Text is of the form:
-    1. Hot Nickel Ball On A P*ssy 0:00
-    2. Are You Serious 2:19
-    ...
+def parse(file, time_i, title_i_start, title_i_end, final_time_end):
+    """Parse lines using given arguments.
+
+    file: An iterable where each iteration yields a line.
+    time_i: The index of the timestamp. Supports negative indices.
+    title_i_start: The index of the first word of the chapter's title.
+    title_i_end: The index of the end of the chapter's title.
+    final_time_end: The ending time of the final chapter, which is the length of the track.
     """
     file_iter = iter(file)
     tokens = next(file_iter).split()
     try:
         while True:
-            time_start = tokens[-1]
-            chapter_string = ' '.join(tokens[1:-1])
+            time_start = tokens[time_i]
+            if not time_string(time_start):
+                print('error: could not interpret \'{}\' as time'
+                      ''.format(time_start))
+                sys.exit(1)
+            chapter_string = ' '.join(tokens[title_i_start:title_i_end])
             tokens = next(file_iter).split()
             yield {
                 'time_start': time_start,
-                'time_end': tokens[-1],
+                'time_end': tokens[time_i],
                 'chapter_string': chapter_string
             }
     except StopIteration:
         yield {
             'time_start': time_start,
-            'time_end': END_TIME,
+            'time_end': final_time_end,
             'chapter_string': chapter_string
         }
 
-def parse_mario_galaxy(file):
-    """From the description of https://www.youtube.com/watch?v=ca7IELhD_mI
-    Text is of the form:
-    0:00 Wii Menu - Starting The Game (''Super Mario Galaxy'')
-    0:04 Star Festival
-    ...
-    """
-    file_iter = iter(file)
-    tokens = next(file_iter).split()
-    try:
-        while True:
-            time_start = tokens[0]
-            chapter_string = ' '.join(tokens[1:])
-            tokens = next(file_iter).split()
-            yield {
-                'time_start': time_start,
-                'time_end': tokens[0],
-                'chapter_string': chapter_string
-            }
-    except StopIteration:
-        yield {
-            'time_start': time_start,
-            'time_end': END_TIME,
-            'chapter_string': chapter_string
-        }
+def main():
+    # Configure argparse
+    parser = argparse.ArgumentParser(
+            description="Converts lines of text containing chapter information "
+                        "into XML readable by mkvpropedit.")
+    parser.add_argument('-d', '--debug', action='store_true', help="enable "
+                        "debug printing")
+    parser.add_argument('inputfile', help="text file where each line has a "
+                        "chapter name and start time")
+    parser.add_argument('outputfile', help="XML file to be written to")
+    parser.add_argument('-i', dest='indices', metavar=('timestamp', 'titlestart',
+                        'titleend'), nargs=3, help="specifies the indices of the "
+                        " elements on each line (supports negative indices) "
+                        "where timestamp=tokens[timestamp] and title=tokens"
+                        "[titlestart:titleend]. Arguments must be integers or "
+                        "None. If neither -i nor -c are specified, an indexing "
+                        "configuration will be automatically generated.")
+    parser.add_argument('-c', dest='config', help="Use a premade indexing "
+                        "configuration. Overrides the -i option. Valid "
+                        "configs are: {}".format(', '.join(CONFIG_PRESETS)), 
+                        choices=CONFIG_PRESETS, metavar='config', default=None)
+    parser.add_argument('-e', help="specifies the timestamp of the end of the "
+                        "final chapter, usually the length of the entire track.",
+                        dest='endtime', required=True, metavar='endtime')
+    # Parse args
+    args = parser.parse_args()
+    INPUT_FILENAME = args.inputfile
+    OUTPUT_FILENAME = args.outputfile
+    debug = args.debug
+    if args.config is not None:
+        try:
+            parse_config = CONFIG_PRESETS[args.config]
+        except KeyError:
+            # Should theoretically never get here
+            print('config not recognized: {}'.format(args.config))
+            sys.exit(1)
+    elif args.indices is not None:
+        try:
+            parse_config = [None if i.lower() == 'none' else int(i) for i in args.indices]
+        except ValueError as ve:
+            print('error: could not interpret {} as index'.format(ve.args[0].split()[-1]))
+            sys.exit(1)
+    else:
+        if debug:
+            print('no configuration specified, generating...')
+        print('automatic configuration generation not yet implemented, please '
+              ' use -i or -c')
+        sys.exit(1)
+    if time_string(args.endtime):
+        END_TIME = args.endtime
+    else:
+        print('error: could not interpret \'{}\' as time'.format(args.endtime))
+        sys.exit(1)
+    # Create elementTree
+    chapters = etree.Element('Chapters')
+    edition_entry = etree.SubElement(chapters, 'EditionEntry')
+    for tokens in parse(open(INPUT_FILENAME, encoding='utf-8'), *parse_config, END_TIME):
+        # Iterate through each chapter
+        if debug:
+            print('processing chapter {0[chapter_string]}, start={0[time_start]}, '
+                  'end={0[time_end]}'.format(tokens))
+        chapter = etree.SubElement(edition_entry, 'ChapterAtom')
+        time_start = etree.SubElement(chapter, 'ChapterTimeStart')
+        time_start.text = tokens['time_start']
+        time_end = etree.SubElement(chapter, 'ChapterTimeEnd')
+        time_end.text = tokens['time_end']
+        chapter_display = etree.SubElement(chapter, 'ChapterDisplay')
+        chapter_string = etree.SubElement(chapter_display, 'ChapterString')
+        chapter_string.text =  tokens['chapter_string']
+        chapter_language = etree.SubElement(chapter_display, 'ChapterLanguage')
+        chapter_language.text = 'eng'
+    # Output XML
+    xml_bytes = etree.tostring(chapters, pretty_print=True, xml_declaration=True)
+    if debug:
+        print('opening file {}'.format(OUTPUT_FILENAME))
+    with open(OUTPUT_FILENAME, 'w') as file_out:
+        file_out.write(xml_bytes.decode())
+    if debug:
+       print('wrote to {} succesfully'.format(OUTPUT_FILENAME))
+    sys.exit(0)
 
-parse = parse_mario_galaxy
-
-chapters = etree.Element('Chapters')
-edition_entry = etree.SubElement(chapters, 'EditionEntry')
-
-for tokens in parse(open(INPUT_FILENAME, encoding='utf-8')):
-    chapter = etree.SubElement(edition_entry, 'ChapterAtom')
-    time_start = etree.SubElement(chapter, 'ChapterTimeStart')
-    time_start.text = tokens['time_start']
-    time_end = etree.SubElement(chapter, 'ChapterTimeEnd')
-    time_end.text = tokens['time_end']
-    chapter_display = etree.SubElement(chapter, 'ChapterDisplay')
-    chapter_string = etree.SubElement(chapter_display, 'ChapterString')
-    chapter_string.text =  tokens['chapter_string']
-    chapter_language = etree.SubElement(chapter_display, 'ChapterLanguage')
-    chapter_language.text = 'eng'
-
-out_str = etree.tostring(chapters, pretty_print=True, xml_declaration=True).decode()
-
-with open(OUTPUT_FILENAME, 'w') as file_out:
-    file_out.write(out_str)
+if __name__ == '__main__':
+    sys.argv.extend('-h'.split())
+    main()
